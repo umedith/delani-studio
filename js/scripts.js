e25cf6a6355269dfcea5edfceea843795e0088e5
umedith <umedith03@gmail.com>
2020-10-04 23:57:35 +0200

    add the last section of contact us

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,31 @@
-$(document).ready(function() {
-    $(".clickable").click(function() {
-      $(".image-fluid").toggle();
-      $(".image-fluid").toggle();
-    });
-  });
+
+                            $(document).ready(function () {
+                               $('.three').click(function () {
+                                    $('.text-three').show();
+                                    $('.three').hide();
+                               });
+                               $('.text-three').click(function () {
+                                    $('.three').show();
+                                    $('.text-three').hide();
+                               });
+                          });
+                          $(document).ready(function () {
+                               $('.four').click(function () {
+                                    $('.text-four').show()
+                                    $('.four').hide()
+                               })
+                               $('.text-four').click(function () {
+                                    $('.four').show()
+                                    $('.text-four').hide()
+                               });
+                          });
+                          $(document).ready(function () {
+                               $('.five').click(function () {
+                                    $('.text-five').show()
+                                    $('.five').hide()
+                               })
+                               $('.text-five').click(function () {
+                                    $('.five').show()
+                                    $('.text-five').hide()
+                               });
+                           });
